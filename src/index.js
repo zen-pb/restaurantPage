91faef6f03c5c3bdbe2bf06d296815609e0339e0
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-01 01:17:59 +0800

    Add handler when button is clicked

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./utils.css";
+import "./styles.css";
+import loadHome from "./home";
+import loadMenu from "./menu";
+import loadAbout from "./about";
+
+const content = document.getElementById("content");
+
+const buttons = document.querySelectorAll("nav button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const id = e.target.id;
+    content.innerHTML = "";
+
+    if (id === "home") loadHome();
+    if (id === "menu") loadMenu();
+    if (id === "about") loadAbout();
+
+    
+  });
+});
