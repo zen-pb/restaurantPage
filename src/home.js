551b121b5187356a047afb738b2b2517043cb93b
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-01 12:51:24 +0800

    Add content in home.js

--- src/home.js
+++ src/home.js
@@ -1,3 +1,24 @@
+import heroImg from "./hero.svg";
+
 export default function loadHome() {
   const content = document.getElementById("content");
+
+  const divElement = document.createElement("div");
+  const imgElement = document.createElement("img");
+  const h1Element = document.createElement("h1");
+  const buttonElement = document.createElement("button");
+
+  divElement.classList.add("hero");
+
+  buttonElement.id = "menu";
+
+  imgElement.src = heroImg;
+  h1Element.textContent = "Jujutsu Kaisen x Akihabara Cafe Collaboration";
+  buttonElement.textContent = "Explore Cursed Delicacies";
+
+  divElement.appendChild(imgElement);
+  divElement.appendChild(h1Element);
+  divElement.appendChild(buttonElement);
+
+  content.appendChild(divElement);
 }
