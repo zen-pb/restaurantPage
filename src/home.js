be590ae6f2967aecb580029df440fb9ef706a873
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-03 18:19:06 +0800

    Add another cta button

--- src/home.js
+++ src/home.js
@@ -6,19 +6,29 @@ export default function loadHome() {
   const divElement = document.createElement("div");
   const imgElement = document.createElement("img");
   const h1Element = document.createElement("h1");
+  const btnDiv = document.createElement("div");
   const buttonElement = document.createElement("button");
+  const aboutBTN = document.createElement("button");
 
   divElement.classList.add("hero");
+  btnDiv.classList.add("btn-div");
+  aboutBTN.classList.add("about-us");
 
   buttonElement.id = "menu";
+  aboutBTN.id = "about";
 
   imgElement.src = heroImg;
   h1Element.textContent = "Jujutsu Kaisen x Akihabara Cafe Collaboration";
   buttonElement.textContent = "Explore Cursed Delicacies";
+  aboutBTN.textContent = "About the Cafe";
 
   divElement.appendChild(imgElement);
   divElement.appendChild(h1Element);
-  divElement.appendChild(buttonElement);
+
+  btnDiv.appendChild(buttonElement);
+  btnDiv.appendChild(aboutBTN);
+
+  divElement.appendChild(btnDiv);
 
   content.appendChild(divElement);
 }
