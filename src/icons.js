4d258c9fa9461edfa84e21ffc0185d19155e9670
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-01-03 21:29:03 +0800

    Add icon handler

---
+++ src/icons.js
@@ -0,0 +1,6 @@
+import akihabaraLogo from "./akihabara-logo.svg";
+import instagramIcon from "./instagram-icon.svg";
+import locationIcon from "./location-icon.svg";
+import whatsappIcon from "./whatsapp-icon.svg";
+
+export { akihabaraLogo, instagramIcon, locationIcon, whatsappIcon };
